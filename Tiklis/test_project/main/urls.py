9748eea518d06634c4c439830bbf9ab1e5adf9f4
2212072 <2212072@slu.edu.ph>
2023-08-01 14:41:51 +0800

    Linked webpages

--- Tiklis/test_project/main/urls.py
+++ Tiklis/test_project/main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
 path("", views.home, name="home"),
 path("home/", views.home, name="home"),
-path("create/", views.create, name="predict"),
-path("test/",views.test, name="test"),
+path("predict/", views.predict, name="predict"),
+path("weather/", views.weather, name="weather"),
+path("crops/",views.crops, name="crops"),
 ]
